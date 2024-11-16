ce357fc16f42517f8542b6835cc9552dabb18238
Rishal-pc7 <rishal01010@gmail.com>
2024-11-16 20:29:04 +0530

    Design changes and public folder name changes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import { Copyright, FacebookIcon, InstagramIcon, LinkedinIcon, Twitter, TwitterI
 import footerBg from '../../assets/footer-bg.jpg'
 function Footer() {
   return (
-         <div className="relative h-[135vh] md:h-[65vh]  text-white ">
+         <div className="relative h-[125vh] md:h-[65vh]  text-white ">
         <div className="w-full h-full absolute opacity-80" style={{backgroundImage:`url(${footerBg})`,backgroundPosition:'center',backgroundSize:'cover'}} >
 
         </div>
@@ -19,11 +19,11 @@ function Footer() {
       <div className="flex flex-wrap gap-y-10 md:flex-nowrap z-50  justify-between mt-4  md:gap-x-10 px-2 md:px-10 text-white pb-10">
 
       
-      <div className="flex flex-col md:flex-row justify-center text-lg md:justify-start items-start gap-x-10 basis-full md:basis-[75%] ">
+      <div className="flex flex-col md:flex-row justify-center  md:justify-start items-start gap-x-10 basis-full md:basis-[75%] ">
          
          <h1 className="text-2xl md:text-2xl font-bold md:basis-1/3 block md:hidden text-white rounded-br border-b-4 pb-4 border-brand-cyan ">Our Products</h1>
          
-         <ul className="flex flex-col items-start w-full leading-relaxed text-justify mt-5 md:mt-0 text-base md:text-lg" style={{wordBreak:'break-word',whiteSpace:'nowrap'}}>
+         <ul className="flex flex-col  items-start w-full leading-relaxed text-justify mt-5 md:mt-0 text-lg" style={{wordBreak:'break-word',whiteSpace:'nowrap'}}>
         
          <li className='underline-offset-2 hover:underline focus:underline'><a href="/products/building_materials">Building Material</a></li>
                     <li className='underline-offset-2 hover:underline focus:underline'><a href='/products/construction_materials'> Construction Material</a></li>
@@ -35,7 +35,7 @@ function Footer() {
                     <li className='underline-offset-2 hover:underline focus:underline'><a href='/products/electrical_and_plumbing_materials'> Electrical and Plumbing Materials </a></li>
          
          </ul>
-         <ul className='flex flex-col items-start w-full leading-relaxed text-justify text-base md:text-lg' style={{wordBreak:'break-word',whiteSpace:'nowrap'}}>
+         <ul className='flex flex-col items-start w-full leading-relaxed text-justify text-lg' style={{wordBreak:'break-word',whiteSpace:'nowrap'}}>
                     <li className='underline-offset-2 hover:underline focus:underline'><a href="/products/valves_&_pumps">Valves & Pumps</a></li>
                     <li className='underline-offset-2 hover:underline focus:underline'><a href='/products/finishing_materials'>Finishing Material</a></li>
                     <li className='underline-offset-2 hover:underline focus:underline'><a href='/products/air_conditioning_&_HVAC_equipments'> Air Conditioning & HVAC Equipments </a></li>
