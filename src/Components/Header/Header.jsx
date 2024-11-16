2b913f85ae2f83f73f807448baa073c3719c550e
Rishal-pc7 <rishal01010@gmail.com>
2024-11-16 11:37:54 +0530

    Header Design changed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -71,7 +71,7 @@ function Header() {
     <div className="logo h-auto w-full z-10 md:h-24 bg-transparent absolute top-0 left-0 flex justify-between">
     
       <img className="h-12 m-2 md:h-28 md:m-10 w-auto md:inline " src={logo} alt=""/>
-      <div className="menus  md:hidden flex justify-end w-1/3">
+      <div className="menus  md:hidden flex justify-end w-[20%]">
             {menu ? <button className='outline-none p-4 bg-none' onClick={()=>setMenu(!menu)}><X/></button>:<button className='outline-none p-4 bg-none' onClick={()=>setMenu(!menu)}><MenuIcon/></button>}
         </div>     
 
