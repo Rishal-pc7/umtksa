d912d15b4f609cfe9116522d34b9cea9c8b735d4
Rishal-pc7 <rishal01010@gmail.com>
2024-11-23 19:34:27 +0530

    Preloader Setting

--- src/Components/Preloader.jsx
+++ src/Components/Preloader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Preloader() {
   return (
-    <div className='w-full h-full absolute bottom-0 bg-red-700 top-0 left-0'>
+    <div className='w-full h-full absolute bottom-0 bg-white top-0 left-0'>
        
     </div>
   )
